d3e0fcf2c33d4b8db0db2653d0e8c48b4e86ada9
omar <omarion-hip@hotmail.com>
2020-11-05 15:43:43 -0500

    Creacion nivel 1 Validacion nombre de jugador Musica de fondo

--- app/src/main/java/com/uisrael/fruitkid/MainActivity.java
+++ app/src/main/java/com/uisrael/fruitkid/MainActivity.java
@@ -2,17 +2,23 @@ package com.uisrael.fruitkid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText et_nombre;
     private ImageView iv_personaje;
     private TextView tv_bestScore;
     private MediaPlayer mp;
+
 
     //Se genera un numero y aparece una imagen diferente cada vez que se abre la app
     int num_aleatorio = (int) (Math.random() * 10);
@@ -47,5 +53,32 @@ public class MainActivity extends AppCompatActivity {
             id = getResources().getIdentifier("uva", "drawable", getPackageName());
             iv_personaje.setImageResource(id);
         }
+
+        //Musica de fondo
+        mp = MediaPlayer.create(this,R.raw.alphabet_song);
+        mp.start();
+        mp.setLooping(true);
+    }
+
+    public void jugar(View v){
+        String nombre = et_nombre.getText().toString();
+        if(!nombre.equals("")){
+            mp.stop();
+            mp.release(); //Libera los recursos
+            Intent i = new Intent(this, activityNivel1.class);
+            i.putExtra("jugador", nombre);
+            startActivity(i);
+            finish();
+        }else{
+            Toast.makeText(this, "Debes escribir tu nombre ", Toast.LENGTH_SHORT).show();
+            //Se abre el teclado y escribe en el editText
+            et_nombre.requestFocus();
+            InputMethodManager imm = (InputMethodManager)getSystemService(this.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(et_nombre, InputMethodManager.SHOW_IMPLICIT);
+
+        }
+    }
+    @Override
+    public void onBackPressed(){
     }
 }
---
+++ app/src/main/java/com/uisrael/fruitkid/activityNivel1.java
@@ -0,0 +1,14 @@
+package com.uisrael.fruitkid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityNivel1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nivel1);
+    }
+}
